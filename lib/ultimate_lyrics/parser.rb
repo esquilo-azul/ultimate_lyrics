c009dbfad8e75bb227fb1f8555223814264abaea
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2021-09-20 19:01:02 -0300

    UltimateLyrics::Parser.

---
+++ lib/ultimate_lyrics/parser.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+require 'eac_ruby_utils/core_ext'
+
+module UltimateLyrics
+  class Parser
+    common_constructor :provider, :original_content
+
+    def original_content_invalid?
+      provider.invalid_indicators.any? { |v| original_content.include?(v) }
+    end
+
+    def result
+      return nil if original_content_invalid?
+
+      sanitize_text(provider.rules.inject(original_content) { |a, e| e.apply(a) })
+    end
+
+    def sanitize_text(text)
+      r = text.to_s.gsub("\t", ' ').gsub("\r", '').gsub(/ +/, ' ').gsub(/\n{2,}/m, "\n\n").strip
+      r.present? ? "#{r}\n" : nil
+    end
+
+    def url?
+      provider.extract_rules.any?(&:url?)
+    end
+  end
+end
---
+++ spec/lib/ultimate_lyrics/parser_spec.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+require 'ultimate_lyrics/parser'
+require 'ultimate_lyrics/provider'
+
+::RSpec.describe ::UltimateLyrics::Parser do
+  include_examples 'source_target_fixtures', __FILE__
+
+  def source_data(source_file)
+    parser = described_class.new(
+      provider_by_basename(::File.basename(source_file)),
+      ::File.read(source_file)
+    )
+    { url: parser.url?, result: parser.result }
+  end
+
+  def provider_by_basename(basename)
+    ::UltimateLyrics::Provider.by_identifier(basename.split('__').first)
+  end
+end
